bdf9aa34e7e0778321f99c914cff5d3a2813eb74
Firomsa Abdurahman <v-fiabd@microsoft.com>
2019-09-04 15:56:26 -0700

    first poc

---
+++ POCSample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+
+namespace POCSample
+{
+
+    using CommandLine;
+
+    using System;
+    /// <summary>
+
+    /// Main samples entry point.
+
+    /// </summary>
+    /// 
+
+    class BackEndOptions
+
+    {
+
+        [Option('u', "uri", HelpText = "Please specify the uri of backend", Required = true)]
+
+        public string Uri { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Parser.Default.ParseArguments<BackEndOptions>(args)
+
+             .MapResult(
+
+                 (BackEndOptions options) => TestMSCore.Run(options.Uri),
+
+                 errs => 1);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ POCSample/TestMSCore.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Graph;
+
+
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+using OpenCensus.Exporter.Zipkin;
+using OpenCensus.Trace;
+using OpenCensus.Trace.Config;
+using OpenCensus.Trace.Sampler;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POCSample
+{
+    
+  public class TestMSCore
+    {
+        public static object Run(string zipkinUri)
+        {
+            
+        // 1. Configure exporter to export traces to Zipkin
+        var exporter = new ZipkinTraceExporter(
+            new ZipkinTraceExporterOptions()
+            {
+                Endpoint = new Uri(zipkinUri),
+                ServiceName = "trace-A",
+            },
+            Tracing.ExportComponent);
+
+            exporter.Start();
+            
+
+            // 2. Configure 100% sample rate for the purposes of the demo
+            ITraceConfig traceConfig = Tracing.TraceConfig;
+            ITraceParams currentConfig = traceConfig.ActiveTraceParams;
+            var newConfig = currentConfig.ToBuilder()
+                .SetSampler(Samplers.AlwaysSample)
+                .Build();
+            traceConfig.UpdateActiveTraceParams(newConfig);
+            
+
+            //Build application
+            // Microsoft.Identity.Client
+            var confidentialClientApplication = PublicClientApplicationBuilder
+                .Create("efe9af1d-0c2a-499e-bc4c-1d23c22a8a87")
+                .Build();
+
+            // Microsoft.Graph.auth
+            DeviceCodeProvider authenticationProvider = new DeviceCodeProvider(confidentialClientApplication);
+
+  
+
+            var config = new GraphTelemetryConfiguration();
+            config.LatencyConfig = new LatencyConfig {
+                authenticationLatencyIsEnabled = true,
+                compressionHandlerLatencyIsEnabled = true,
+
+            };
+     
+             var client = new GraphServiceClient(authenticationProvider: authenticationProvider, config: config);
+     
+
+            using (var scope = Tracing.Tracer.SpanBuilder("'v1.0/me' full request latency ").StartScopedSpan())
+            {
+                var user = client.Me.Request().GetAsync().GetAwaiter().GetResult();
+
+                Console.WriteLine("Succesfully captured full latency of /me");
+            }
+
+
+            //using (var scope = Tracing.Tracer.SpanBuilder("v1.0/me/Drive/Root/Children ").StartScopedSpan())
+            //{
+            //    var driveItemsStream = client.Me.Drive.Items["01ZJ3LUMVJPVXOFF43LVA23PKLT3KRFL54"].Content.Request().GetAsync().GetAwaiter().GetResult();
+                
+            //}
+
+
+
+            Tracing.ExportComponent.SpanExporter.Dispose();
+
+            return null;
+        }
+
+    }
+}
+
+
+
+
